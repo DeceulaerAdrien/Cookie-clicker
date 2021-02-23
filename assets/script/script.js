a4b8dae2172d78a5bb66986d52d6c6e5883d81ce
zakaria <zakariaselassi@gmail.com>
2021-02-23 10:17:59 +0100

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,34 +1,36 @@
-let score = new Number (document.getElementById('score').textContent);
+let score = new Number(document.getElementById('score').textContent);
 let x = 1;
 let y = 1;
 
-document.getElementById('click').addEventListener('click',()=>{
+document.getElementById('click').addEventListener('click', () => {
     score += x;
-    document.getElementById('score').innerHTML = score ;
-});
-document.getElementById('bonusOne').addEventListener('click',()=>{
-    if(score >= (x *(x + 1)*25)){
-        score = score - (x *(x + 1)*25);
-        document.getElementById('score').innerHTML = score ;
-        x += 1;
+    document.getElementById('score').innerHTML = score;
+    if (score >= 100) {
+        document.getElementById("castor").src = "./assets/images/castor1.png";
     }
-    else{
+
+});
+document.getElementById('bonusOne').addEventListener('click', () => {
+    if (score >= (x * (x + 1) * 25)) {
+        score = score - (x * (x + 1) * 25);
+        document.getElementById('score').innerHTML = score;
+        x += 1;
+    } else {
         x = x;
     }
-    document.getElementById('bonusOne').innerHTML = (x *(x + 1)*25) + ` <br> x${x*2} `;
+    document.getElementById('bonusOne').innerHTML = (x * (x + 1) * 25) + ` <br> x${x*2} `;
 });
 
-document.getElementById('bonusTwo').addEventListener('click',()=>{
+document.getElementById('bonusTwo').addEventListener('click', () => {
 
-    if(score >= (y *(y + 1)*10)){
-        score = score - (y *(y + 1)*10);
-        document.getElementById('score').innerHTML = score ;
+    if (score >= (y * (y + 1) * 10)) {
+        score = score - (y * (y + 1) * 10);
+        document.getElementById('score').innerHTML = score;
         y += 1;
-        setInterval(function () { 
+        setInterval(function() {
             document.getElementById('click').click();
-        },10000/y);        
-    }
-    else{
+        }, 10000 / y);
+    } else {
         y = y;
     }
     document.getElementById('bonusTwo').innerHTML = `${(y *(y + 1)*10)} <br> autoclick`;
