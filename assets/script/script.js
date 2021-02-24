28292807099e6ddc17ffe59d96fb9b75cf47b0b8
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-02-24 09:30:56 +0100

    version 2

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,57 +1,26 @@
-let score = new Number(document.getElementById('score').textContent);
-let x = 1;
-let y = 1;
-let health = 15;
-let cpt = 0;
-document.getElementById('click').addEventListener('click', () => {
-    cpt++;
-    score += x;
-    document.getElementById('score').innerHTML = `<img src="./assets/images/buche.png">  ${score}`;
+let score = 30000/* new Number(document.getElementById('score').textContent) */;
 
-    if (cpt >= 10) {
-        document.getElementById("castor").src = "./assets/images/castor1.png";
-
+document.getElementById('clickerMain').addEventListener('click',() => {
+    score ++
+    document.getElementById('score').innerHTML = `${score}`
+    if(score >= 1000){
+        document.getElementById('castor').src = "./assets/images/castor1.png"
     }
-    if (cpt >= 20) {
-        document.getElementById("castor").src = "./assets/images/castor2.png";
+    if(score >= 10000){
+        document.getElementById('castor').src = "./assets/images/castor2.png"
     }
+})
+let auto = 1;
+document.getElementById('mult1').addEventListener('click', () => {
 
-});
-
-document.getElementById('bonusHealth').addEventListener("click", () => {
-
-    if (cpt >= health) {
-        score = score - health;
-        document.getElementById('score').innerHTML = score;
-        document.getElementById("castor").src = "./assets/images/castor.png";
-        document.getElementById('bonusHealth').innerHTML = `<img src="./assets/images/healthkit.png"> ${health *= 2} points`;
-        console.log(health);
-        cpt = 0;
-    }
-});
-
-document.getElementById('bonusOne').addEventListener('click', () => {
-    if (score >= (x * (x + 1) * 25)) {
-        score = score - (x * (x + 1) * 25);
-        document.getElementById('score').innerHTML = `<img src="./assets/images/buche.png">  ${score}`;
-        x += 1;
-    } else {
-        x = x;
-    }
-    document.getElementById('bonusOne').innerHTML = (x * (x + 1) * 25) + ` <br> x${x*2} `;
-});
-
-document.getElementById('bonusTwo').addEventListener('click', () => {
-
-    if (score >= (y * (y + 1) * 10)) {
-        score = score - (y * (y + 1) * 10);
-        document.getElementById('score').innerHTML = `<img src="./assets/images/buche.png">  ${score}`;
-        y += 1;
+    if (score >= (auto * (auto + 1) * 50)) {
+        score = score - (auto * (auto + 1) * 50);
+        document.getElementById('score').innerHTML = `${score}`;
+        auto += 1;
         setInterval(function() {
-            document.getElementById('click').click();
-        }, 10000 / y);
+            document.getElementById('clickerMain').click();
+        }, 10000 / auto);
     } else {
-        y = y;
+        auto = auto;
     }
-    document.getElementById('bonusTwo').innerHTML = `<img src="./assets/images/hammer.png"> ${(y *(y + 1)*10)} <br> autoclick`;
 });
