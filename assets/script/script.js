0c4730036e75acaa4708e2a19e4172aced2adced
zakaria <zakariaselassi@gmail.com>
2021-02-24 12:20:26 +0100

    Update file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
-let score = 30000 /* new Number(document.getElementById('score').textContent) */ ;
-
+let score = 100000 /* new Number(document.getElementById('score').textContent) */ ;
+let cursor = 1;
 document.getElementById('clickerMain').addEventListener('click', () => {
-    score++
+    score++;
     document.getElementById('score').innerHTML = `${score}`
     if (score >= 1000) {
         document.getElementById('castor').src = "./assets/images/castor1.png"
@@ -10,17 +10,18 @@ document.getElementById('clickerMain').addEventListener('click', () => {
         document.getElementById('castor').src = "./assets/images/castor2.png"
     }
 })
-let auto = 1;
-document.getElementById('mult1').addEventListener('click', () => {
 
-    if (score >= (auto * (auto + 1) * 50)) {
-        score = score - (auto * (auto + 1) * 50);
+document.getElementById('marteau').addEventListener('click', () => {
+
+    if (score >= (cursor * (cursor + 1) * 50)) {
+        score = score - (cursor * (cursor + 1) * 50);
         document.getElementById('score').innerHTML = `${score}`;
-        auto += 1;
+        document.getElementById('marteau').innerHTML = `<img src="./assets/images/2087470.svg"> ${cursor}  ` + (cursor * (cursor + 1) * 50);
+        cursor += 1;
         setInterval(function() {
             document.getElementById('clickerMain').click();
-        }, 10000 / auto);
+        }, 10000 / cursor);
     } else {
-        auto = auto;
+        cursor = cursor;
     }
 });
